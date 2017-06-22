dde8808694434b19c1d2d07f9f35572d7e55099a
Island <764796124@qq.com>
2017-06-23 00:15:12 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,27 +1,32 @@
 @echo off
-echo 'step 1:��ȡ����ip��ַ'
+echo 'step 1:get your local ip'
 pause
 for /f "tokens=2 delims=:" %%a in ('ipconfig^|findstr "IPv4"') do (
  set IP=%%a
 )
-echo var baseUrl='http://%IP%:50141';export {baseUrl} > ./config/ipconfig.js
+echo var baseUrl='http://%IP:~1%:50141';export {baseUrl} > ./config/ipconfig.js
 
-echo 'step 2.��װnode.js,��ʹ��Ĭ�����ð�װ��c:/program files, ��װ�����ִ��step3'
+echo 'step install װnode.js, please install nodejs in the c:/program files'
 echo.&echo
 echo   1.32bit
 echo   2.64bit
 
-set /p select=���������� ���س�����:
+set /p select= choose your version:
 if %select%== 1 (start "" "./node-v8.1.2-x86.msi")
 if %select%== 2 (start "" "./node-v8.1.2-x64.msi"
 )
 pause
 
-echo 'step 4:����node��������'
+echo 'step 4:config nodejs'
 pause
 
+echo set path=%path%;C:\Program Files (x86)\Nodejs
+echo set path=%path%;C:\Program Files (x86)\nodejs
+echo set path=%path%;C:\Program Files\Nodejs
 echo set path=%path%;C:\Program Files\nodejs
 pause
 
-echo 'step 5:��װ����������ǰ��server'
+echo 'step 5:install and start server'
 pause
+
+npm install && npm start
